11a38956fcc6fd9ccefa6e5fb4020353cb169bcd
hadrienm <adrien.heros@epitech.eu>
2021-10-01 14:56:20 +0200

    [MP3] (AH) : navigation bar has just been added on music plqyer page

--- epi_flutter_1/lib/pages/mp3_player_page.dart
+++ epi_flutter_1/lib/pages/mp3_player_page.dart
@@ -1,3 +1,4 @@
+import 'package:epi_flutter_1/widgets/drawer_music_player_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -14,7 +15,15 @@ class _Mp3Player extends State<Mp3Player> {
         title: const Text('MP3Player'),
         centerTitle: true,
         backgroundColor: Colors.pinkAccent,
-      )
+      ),
+        drawer: const DrawerMusicPlayerWidget(),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[],
+        ),
+      ),
     );
   }
 }
+
---
+++ epi_flutter_1/lib/widgets/drawer_music_player_widget.dart
@@ -0,0 +1,32 @@
+import 'package:epi_flutter_1/components/tile_menu.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DrawerMusicPlayerWidget extends StatelessWidget {
+  final padding = const EdgeInsets.symmetric(horizontal: 12);
+  const DrawerMusicPlayerWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Material(
+        color: const Color.fromRGBO(50, 70, 205, 1),
+        child: ListView(
+          padding: padding,
+          children: <Widget>[
+            const SizedBox(height: 65),
+            TileMenu(
+                text:"Home page",
+                icon: Icons.people,
+                color: const Color.fromRGBO(255, 255, 255, 1),
+                hoverColor: const Color.fromRGBO(250, 250, 250, 1),
+                onClicked: () {
+                  Navigator.of(context).pushNamed("/");
+                }
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
