dfbed88dac1eeee9b34582be82516aef1610fe5d
hadrienm <adrien.heros@epitech.eu>
2021-10-02 18:32:54 +0200

    [MP3] (AH) : List Scrolling has been added

--- epi_flutter_1/lib/pages/mp3_player_page.dart
+++ epi_flutter_1/lib/pages/mp3_player_page.dart
@@ -1,4 +1,3 @@
-import 'package:epi_flutter_1/widgets/ButtonHoverMusicPlayer.dart';
 import 'package:epi_flutter_1/widgets/drawer_music_player_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -22,30 +21,36 @@ class _Mp3Player extends State<Mp3Player> {
         backgroundColor: Colors.grey,
       ),
       drawer: const DrawerMusicPlayerWidget(),
-      body: Center(
-        child: Container(
-          width: width / 1.35,
-          height: height / 1.35,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(60),
-              color: Colors.white,
-              boxShadow: [
-                new BoxShadow(
-                  blurRadius: 12,
-                  spreadRadius: 5,
-                  color: Colors.grey,
-                )
-              ]),
-          child: buttonsUI(),
-        ),
-      ),
+      body: ListWheelScrollView(
+          itemExtent: height / 3,
+          offAxisFraction: -0.5,
+          diameterRatio: 1.5,
+          children: [
+            Container(
+                width: width / 1.35,
+                height: height / 1.35,
+                decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.all(Radius.circular(30))),
+                child: Center(
+                    child: Image(
+                  image: AssetImage("assets/images/evanescence.jpg"),
+                  width: width / 2,
+                  height: height / 2,
+                ))),
+            Container(
+                width: width / 1.35,
+                height: height / 1.35,
+                decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.all(Radius.circular(30))),
+                child: Center(
+                    child: Image(
+                  image: AssetImage("assets/images/mozart_opera_rock.jpg"),
+                  width: width / 2,
+                  height: height / 2,
+                ))),
+          ]),
     );
   }
-}
-
-buttonsUI() {
-  return Column(
-      children: List.generate(2, (int i) {
-    return ButtonHoverMusicPlayer();
-  }));
 }
