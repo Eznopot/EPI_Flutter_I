dfc16dc93bfa4733f9788ed5c8ff64e2f04c457a
mano <mano.baffie@epitech.eu>
2021-10-11 17:14:26 +0200

    start v1 Form

--- epi_flutter_1/lib/pages/form.dart
+++ epi_flutter_1/lib/pages/form.dart
@@ -1,38 +1,48 @@
+import 'package:epi_flutter_1/widgets/new_form_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FormPage extends StatefulWidget {
-
   @override
   State<FormPage> createState() => _FormPageState();
 }
 
 class _FormPageState extends State<FormPage> {
-
-  void Function test() {
-    TextFormField(
-      decoration: const InputDecoration(
-        hintText: 'Name',
-      ),
-      validator: (String? Value) {
-        if (Value == null || Value.isEmpty) {
-          return 'Please enter some text';
-        }
-        return null;
-      },
-    ),
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: <Widget> [
-
-          ],
-        ),
+    return Container(
+      decoration: const BoxDecoration (
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [Colors.blueAccent, Colors.purple]
+        )
       ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
+          child: Card(
+            color: Colors.white,
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget> [
+                  Row(
+                    children: const <Widget> [
+                      Expanded(child: NewForm(name: 'First Name')),
+                      Expanded(child: NewForm(name: 'Last Name')),
+                    ],
+                  ),
+                  const NewForm(name: 'Number'),
+                  const NewForm(name: 'About', maxLines: 5),
+                ],
+              ),
+            ),
+          ),
+        ),
+      )
     );
   }
 }
+
